6c139688c078dd01dc83384ae8c7240d34b9ae7a
crazjath <50624984+crazjath@users.noreply.github.com>
2022-04-03 17:54:31 -0500

    1

--- node-app/src/server.js
+++ node-app/src/server.js
@@ -1,6 +1,8 @@
+const express = require('express');
+const exphbs = require("express-handlebars");
 const path = require('path');
-const  express = require('express');
-const exp = require('constants');
+
+
 
 
 //Initializations
@@ -9,7 +11,14 @@ const app = express();
 //Settings
 app.set('port',process.env.PORT || 4000);
 app.set('views',path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+}))
 
+app.set('view engine','.hbs')
 
 //Middlewares
 app.use(express.urlencoded({extended:false}));//este es para que cualquier data que se registre por medio de un formulario sea json
@@ -19,7 +28,7 @@ app.use(express.urlencoded({extended:false}));//este es para que cualquier data 
 
 //Routes
 app.get('/',(req,res,next) => {
-    res.send('hello world');
+    res.render('index');
 })
 
 

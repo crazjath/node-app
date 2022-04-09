6267d2bf8eeeb56e5c6a6f8d25f20d1488ef5b36
crazjath <50624984+crazjath@users.noreply.github.com>
2022-04-09 16:15:13 -0500

    final

--- node-app/src/controllers/notes.controller.js
+++ node-app/src/controllers/notes.controller.js
@@ -1,38 +1,43 @@
 const notesCtrl = {};
 const Note = require('../models/Note');
 
-notesCtrl.renderNoteForm = (req,res) => {
+notesCtrl.renderNoteForm = (req, res) => {
     res.render('notes/new-note');
 }
 
-notesCtrl.createNewNote = async(req,res) => {
-    const { title, description  } = req.body;
-    const newNote = new Note({ title, description});
+notesCtrl.createNewNote = async (req, res) => {
+    const { title, description } = req.body;
+    const newNote = new Note({ title, description });
+    newNote.user = req.user.id;
     await newNote.save();
 
-    req.flash('success_msg', 'Note added succesfully'); 
+    req.flash('success_msg', 'Note added succesfully');
     res.redirect('/notes')
 }
 
-notesCtrl.renderNotes = async(req,res) => {
-    const notes = await Note.find().lean();
+notesCtrl.renderNotes = async (req, res) => {
+    const notes = await Note.find({ user: req.user.id }).sort({ date: "desc" }).lean();
     res.render('notes/all-notes', { notes });
 }
 
 notesCtrl.renderEditForm = async (req, res) => {
     const note = await Note.findById(req.params.id).lean();
+    if (note.user != req.user.id) {
+        req.flash("error_msg", "Not Authorized");
+        return res.redirect("/notes");
+    }
     res.render('notes/edit-note', { note });
 };
 
-notesCtrl.updateNote = async(req,res) => { 
-    const { title, description  } = req.body;
-    const note = await Note.findByIdAndUpdate(req.params.id,{ title,description }).lean();
-    
+notesCtrl.updateNote = async (req, res) => {
+    const { title, description } = req.body;
+    const note = await Note.findByIdAndUpdate(req.params.id, { title, description }).lean();
+
     req.flash('success_msg', 'Note updated succesfully');
     res.redirect('/notes')
 }
 
-notesCtrl.deleteNote = async(req,res) => { 
+notesCtrl.deleteNote = async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
     req.flash('success_msg', 'Note deleted succesfully');
     res.redirect('/notes')
